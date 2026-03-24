b99e4ed108e60f98dc2cfd49b15471f749f2f078
kgakwaya <k.gakwaya@alustudent.com>
2026-03-24 20:39:07 +0200

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,14 @@
-import * as bcrypt from "./types.js";
-export = bcrypt;
-export as namespace bcrypt;
+/**
+List of binary file extensions.
+
+@example
+```
+import binaryExtensions = require('binary-extensions');
+
+console.log(binaryExtensions);
+//=> ['3ds', '3g2', …]
+```
+*/
+declare const binaryExtensions: readonly string[];
+
+export = binaryExtensions;
