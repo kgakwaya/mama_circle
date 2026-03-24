99214948e34c729450f9024acac3d053073dcb3f
kgakwaya <k.gakwaya@alustudent.com>
2026-03-24 20:38:13 +0200

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,3 @@
-export declare const balanced: (a: string | RegExp, b: string | RegExp, str: string) => false | {
-    start: number;
-    end: number;
-    pre: string;
-    body: string;
-    post: string;
-} | undefined;
-export declare const range: (a: string, b: string, str: string) => undefined | [number, number];
-//# sourceMappingURL=index.d.ts.map
+import * as bcrypt from "./types.js";
+export = bcrypt;
+export as namespace bcrypt;
